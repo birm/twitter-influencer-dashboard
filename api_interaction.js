ae0c09ad0b88baad3da11e2fcd9a1267caf0a078
Ryan Birmingham <birm@rbirm.us>
2018-02-13 22:47:00 -0500

    Create api_interaction.js

---
+++ api_interaction.js
@@ -0,0 +1,15 @@
+// https://developer.twitter.com/en/docs/tweets/timelines/api-reference/get-statuses-user_timeline.html
+// need auth
+function user_info(username, cb){
+    var xhttp = new XMLHttpRequest();
+    xhttp.onreadystatechange = function() {
+        if (this.readyState == 4 && this.status == 200) {
+           var short = [];
+           var tweets = JSON.parse(xhttp.responseText);
+           tweets.forEach((x) => short.push({time: x.created_at, text: x.text}));
+           cb(short)
+        }
+    };
+    xhttp.open("GET", "https://api.twitter.com/1.1/statuses/user_timeline.json?screen_name=" + username, true);
+    xhttp.send();
+}
